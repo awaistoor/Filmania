b9b7bb831b4dc4e24463cf62d0c7ae8f6f877e0e
Awais Toor <awaistoor@gmail.com>
2023-03-03 16:19:50 +0500

    feat: added blank detail fragment with view model

---
+++ app/src/main/java/com/yaasir/filmania/presentation/detail/ui/DetailFragment.kt
@@ -0,0 +1,27 @@
+package com.yaasir.filmania.presentation.detail.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.yaasir.filmania.R
+import com.yaasir.filmania.databinding.FragmentDetailBinding
+import com.yaasir.filmania.presentation.detail.viewmodel.DetailViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
+    private val viewModel: DetailViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
