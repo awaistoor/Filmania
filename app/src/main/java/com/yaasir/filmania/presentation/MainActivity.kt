ec359bdd94c43ff4867fcaaf73ddb6baf986d5aa
Awais Toor <awaistoor@gmail.com>
2023-03-03 13:47:12 +0500

    feat: added blank activity

---
+++ app/src/main/java/com/yaasir/filmania/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yaasir.filmania.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yaasir.filmania.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
