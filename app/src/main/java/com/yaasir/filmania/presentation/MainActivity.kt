81aee3b2db5ade14a9c1409d63c86f3d5593008e
Awais Toor <awaistoor@gmail.com>
2023-03-03 14:02:00 +0500

    feat: intergrated di hilt

--- app/src/main/java/com/yaasir/filmania/presentation/MainActivity.kt
+++ app/src/main/java/com/yaasir/filmania/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.yaasir.filmania.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yaasir.filmania.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
