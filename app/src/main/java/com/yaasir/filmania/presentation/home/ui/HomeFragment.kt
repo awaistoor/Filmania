858be20254a72ec2373376e916191377f2d435b5
Awais Toor <awaistoor@gmail.com>
2023-03-03 14:37:13 +0500

    feat: added viewmodel for home fragment

--- app/src/main/java/com/yaasir/filmania/presentation/home/ui/HomeFragment.kt
+++ app/src/main/java/com/yaasir/filmania/presentation/home/ui/HomeFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.yaasir.filmania.R
 import com.yaasir.filmania.databinding.FragmentHomeBinding
+import com.yaasir.filmania.presentation.home.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
---
+++ app/src/main/java/com/yaasir/filmania/presentation/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.yaasir.filmania.presentation.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class HomeViewModel : ViewModel() {
+}
